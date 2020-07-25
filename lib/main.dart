a5e78b2042b1520e5e234315f83b99c14e2c0e57
reshadmrrr <reshadmrrr@gmail.com>
2020-07-25 12:27:13 +0600

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Age Calculator',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: null,
+    );
+  }
+}
